c0e2d8f39dbc704c08af8301c196ae0119992e6e
Anton Kolimbetov <anton.kolimbetov@gmail.com>
2019-01-13 19:36:38 +0600

    Initial Commit

---
+++ lesson9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson9
+//
+//  Created by Anton Kolimbetov on 1/13/19.
+//  Copyright © 2019 Anton Kolimbetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

d5ba7ac7fcc451aec4ee0fbd13fcb20b6c2a13b5
Anton Kolimbetov <anton.kolimbetov@gmail.com>
2019-01-13 20:25:09 +0600

    ok

--- lesson9/ViewController.swift
+++ lesson9/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        lightOn = !lightOn
+        
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
